868184e05298340b342271377f8c1f8749027475
Amit <51166359+amitsharma2099@users.noreply.github.com>
2019-06-07 19:52:53 +0530

    Update SpringBootRestApplication.java

--- src/main/java/com/example/springbootrest/SpringBootRestApplication.java
+++ src/main/java/com/example/springbootrest/SpringBootRestApplication.java
@@ -3,6 +3,16 @@ package com.example.springbootrest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Actuator Endpoints:
+http://localhost:8080/actuator
+http://localhost:8080/actuator/info
+http://localhost:8080/actuator/health
+http://localhost:8080/actuator/env
+http://localhost:8080/actuator/metrics
+http://localhost:8080/actuator/heapdump
+ */
+
 @SpringBootApplication
 public class SpringBootRestApplication {
 
